2026203010e9515bf624017a696b37eef9e5ddff
annonymouse <annonymouse@users.noreply.github.com>
2015-12-26 19:10:18 +0000

    Add deploy script and better readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Dotfiles for homeshick
 
-Just my .hgrc and .gitconfig files for now.
+Underneath this uses homeshick to do all the heavy lifting, but i like to keep
+my vim repo separate from things.
+
+To simplify matters for my own setup, run
+
+```
+git clone https://github.com/annonymouse/dotfiles
+cd dotfiles
+./deploy.sh
+```
+
+Deploy.sh is idempotent if you've already got things installed.
---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -e
+
+HOMESICK_DIR=$HOME/.homesick/repos
+HOMESHICK_PATH=$HOMESICK_DIR/homeshick/bin
+HOMESHICK=$HOMESHICK_PATH/homeshick
+DOTFILES_DIR=$(pwd)/$(dirname $0)
+installHomeshick() {
+    echo "Homeshick not installed, installing..."
+    mkdir -p $HOMESICK_DIR
+    git clone git://github.com/andsens/homeshick.git $HOMESICK_DIR/homeshick
+}
+
+symlinkdotfiles(){
+    echo "Symlinking dotfiles"
+    [ ! -d $HOMESICK_DIR/dotfiles ] || { echo "Dotfiles directory exists" >&2; exit -1 ; }
+    ln -s $DOTFILES_DIR $HOMESICK_DIR/dotfiles
+    $HOMESHICK link dotfiles
+}
+
+deploy(){
+    USER=$1
+    CASTLE=$2
+    $HOMESHICK list | grep "$USER/$CASTLE" && { echo "$CASTLE already exists" >&2; exit -1 ; }
+    $HOMESHICK clone -f $USER/$CASTLE
+    $HOMESHICK link -f $CASTLE
+    $HOMESHICK cd $CASTLE
+    [ ! -x deploy ] || ./deploy
+}
+# Check whether homeshick is installed
+command -v homeshick >/dev/null 2>&1 || [ -d $HOMESICK_DIR ] || installHomeshick
+# Check whether this directory is symlinked
+[ -h $HOMESICK_DIR/dotfiles ] || symlinkdotfiles
+
+# Checkout vimplugins
+deploy annonymouse vim-castle
+
